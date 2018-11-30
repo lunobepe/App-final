c00fcc4c7870f1b9feecac3d6ceafa102b06ec86
Noemi <nunesnoemi17@gmail.com>
2018-11-29 22:43:41 -0200

    Listando perfil

--- app/src/main/java/com/lourdinas/finalaplication/MainActivity.java
+++ app/src/main/java/com/lourdinas/finalaplication/MainActivity.java
@@ -11,6 +11,9 @@ import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity
@@ -30,9 +33,31 @@ public class MainActivity extends AppCompatActivity
         toggle.syncState();
         NavigationView navigationView = findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        TextView tx_nome = findViewById(R.id.nome);
+        TextView tx_sobrenome = findViewById(R.id.sobrenome);
+        TextView tx_formacao = findViewById(R.id.formacao);
+        TextView tx_idade = findViewById(R.id.idade);
+        TextView tx_telefone = findViewById(R.id.telefone);
+        TextView tx_data = findViewById(R.id.data);
+
+        Intent intentRecebido = getIntent();
+        String nome = intentRecebido.getStringExtra("nome");
+        String sobrenome = intentRecebido.getStringExtra("sobrenome");
+        String formacao = intentRecebido.getStringExtra("formacao");
+        String idade = intentRecebido.getStringExtra("idade");
+        String telefone = intentRecebido.getStringExtra("telefone");
+        String data = intentRecebido.getStringExtra("data");
+
+        tx_nome.setText(nome);
+        tx_sobrenome.setText(sobrenome);
+        tx_formacao.setText(formacao);
+        tx_idade.setText(idade);
+        tx_telefone.setText(telefone);
+        tx_data.setText(data);
     }
 
-    @Override
+        @Override
     public void onBackPressed() {
         DrawerLayout drawerLayout = findViewById(R.id.drawer_layout);
         if(drawerLayout.isDrawerOpen(GravityCompat.START)) {
